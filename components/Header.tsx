fe28599ea91f8e9c2a3af68f5431053f6f9c0ec8
Clint Oka <okaclint@gmail.com>
2024-01-13 22:30:16 -1000

    image update

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,11 @@
+import Image from "next/image";
+
 export default function Header() {
     return(
         <div className="flex justify-center">
             <div className="px-4 py-2 w-full flex justify-between max-w-6xl">
             <div className="">
-                <img src="/logo.png" className="rounded-sm" height={50} width={50} />
+                <Image alt="logo" src="/logo.png" className="rounded-sm" height={50} width={50} />
             </div>
             <div className="flex justify-center space-x-4 items-center">
             <a target="_blank" href="https://www.youtube.com/channel/UC_yj0XCaMjFkAojXxbiU-cg"><svg xmlns="http://www.w3.org/2000/svg" height="20" width="22" viewBox="0 0 576 512"><path d="M549.7 124.1c-6.3-23.7-24.8-42.3-48.3-48.6C458.8 64 288 64 288 64S117.2 64 74.6 75.5c-23.5 6.3-42 24.9-48.3 48.6-11.4 42.9-11.4 132.3-11.4 132.3s0 89.4 11.4 132.3c6.3 23.7 24.8 41.5 48.3 47.8C117.2 448 288 448 288 448s170.8 0 213.4-11.5c23.5-6.3 42-24.2 48.3-47.8 11.4-42.9 11.4-132.3 11.4-132.3s0-89.4-11.4-132.3zm-317.5 213.5V175.2l142.7 81.2-142.7 81.2z"/></svg></a>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,11 @@
+import Image from "next/image";
+
 export default function Hero() {
     return(
         <>
         <div className="flex justify-center space-x-2 sm:hidden px-4 pt-10 pb-5">
             <div className="">
-            <img src="plant.jpg" height={400} width={150} />
+            <Image src="/plant.jpg" height={400} width={150} alt="plant" />
             <p className="text-xs">
             by <a href="https://unsplash.com/@georgiadelotz?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Georgia de Lotz</a> 
             </p>
@@ -12,11 +14,11 @@ export default function Hero() {
             <div className="flex flex-col items-center">
             <h3><span className="text-2xl font-thin">Celebrate the</span></h3>
             <h3 className="-mt-2"><span><span className="text-3xl font-extrabold text-green-800"> imperfect</span></span></h3>
-            <img className="mr-auto pt-1" src="/fire.jpg" height={100} width={100} />
+            <Image alt="fire" className="mr-auto pt-1" src="/fire.jpg" height={100} width={100} />
             <p className="text-xs mr-auto">
             by <a href="https://unsplash.com/@cullansmith?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Cullan Smith</a>
             </p>
-            <img className="mr-auto pt-1" src="/wood.jpg" height={100} width={100} />
+            <Image alt="round wood" className="mr-auto pt-1" src="/wood.jpg" height={100} width={100} />
             <p className="mr-auto text-xs">
             by <a href="https://unsplash.com/@theforestbirds?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Førestbird</a> 
             </p>
@@ -24,7 +26,7 @@ export default function Hero() {
         </div>
         <div className="hidden px-10 sm:flex justify-center">
             <div className="pt-32">
-            <img src="/plant.jpg" className="rounded-sm" height={800} width={300} />
+            <Image alt="plant" src="/plant.jpg" className="rounded-sm" height={800} width={300} />
             by <a href="https://unsplash.com/@georgiadelotz?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Georgia de Lotz</a> 
             </div>
             <div className="flex flex-col ml-5 items-center">
@@ -33,12 +35,12 @@ export default function Hero() {
                 <h3><span><span className="text-8xl font-extrabold text-green-800"> imperfect</span></span></h3>
                 </div>
                 <div className="mr-auto pt-6">
-               <img src="/fire.jpg" className="rounded-sm" height={250} width={250} />
+               <Image alt="fire" src="/fire.jpg" className="rounded-sm" height={250} width={250} />
                by <a href="https://unsplash.com/@cullansmith?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Cullan Smith</a>
                 </div>
                 <div className="mr-auto mt-3">
                     <div>
-                        <img src="/wood.jpg" className="rounded-sm" height={250} width={250} />
+                        <Image alt="wood" src="/wood.jpg" className="rounded-sm" height={250} width={250} />
                         by <a href="https://unsplash.com/@theforestbirds?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Joel & Jasmin Førestbird</a> 
   
                     </div>

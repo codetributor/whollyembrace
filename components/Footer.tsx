3239946511c25cfd9b238cd2a413939c5f96a1ed
Clint Oka <okaclint@gmail.com>
2024-01-13 20:21:43 -1000

    initial commit

---
+++ components/Footer.tsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+    return(
+        <div className="flex justify-center w-full bg-white mt-10">
+            <div className="flex w-full flex-col items-center py-5 px-5 sm:flex-row sm:justify-between max-w-6xl">
+            <div>
+                <p className="text-green-800">whollyembrace@gmail.com</p>
+            </div>
+            <div>
+                <p className="text-green-800"><span className="text-gray-400">made with love by</span> Earb Amari</p>
+            </div>
+        </div>
+        </div>
+        
+    );
+}
---
+++ components/SectionOne.tsx
@@ -0,0 +1,18 @@
+export default function SectionOne() {
+    return(
+        <div className="flex justify-center h-full flex-col items-center pt-5 space-y-5 bg-gray-100 sm:pt-20 pb-10 mb-10">
+            <h3 className="font-thin sm:text-2xl">philosophy</h3>
+            <div className="flex flex-wrap sm:grid sm:grid-cols-2 md:grid-cols-3 px-2 justify-center space-y-2 w-full sm:pt-10 sm:space-y-0 sm:gap-x-4 max-w-6xl">
+                <div className="bg-white font-dance text-2xl cursor-pointer hover:text-green-800 text-gray-400 shadow-md hover:scale-105 rounded-sm flex justify-center items-center w-full py-5 sm:py-10">
+                    embrace yourself
+                </div>
+                <div className="bg-white text-2xl font-dance cursor-pointer hover:text-green-800 text-gray-400 shadow-md hover:scale-105 rounded-sm flex justify-center items-center w-full py-5 sm:py-10">
+                    embrace others
+                </div>
+                <div className="bg-white text-2xl font-dance cursor-pointer hover:text-green-800 text-gray-400 shadow-md hover:scale-105 rounded-sm flex justify-center items-center w-full py-5 sm:py-10">
+                    embrace life
+                </div>
+            </div>
+        </div>
+    )
+}

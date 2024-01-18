b3729bb58fe335821512826c088966659f1040c6
Clint Oka <okaclint@gmail.com>
2024-01-17 15:56:46 -1000

    add artifact

--- components/PonoPiece.tsx
+++ components/PonoPiece.tsx
@@ -7,7 +7,7 @@ export default function PonoPiece() {
             <div className="w-full max-w-6xl px-5 flex flex-col items-center space-y-3 sm:space-y-0 sm:flex-row sm:justify-between">
                 <div className="flex flex-col items-center max-w-xs">
                 <Image src="/pono.png" height={50} width={300} alt="logo" />
-                <p className="text-center text-white">is a local writers group to help each other create thier masterpiece</p>
+                <p className="text-center text-white">is a local writers group to help each other create thier masterpiece or artifact</p>
                 </div>
                 
                 <Link target="_blank" href="https://ponopiece.com"><div className="bg-gray-200 px-4 py-2 rounded-md hover:scale-105 text-green-800 text-center w-fit">explore</div></Link>
